5548914a3a027b442b6976c5f0e08557fcc66a5f
Max Brandt <mbrandt@shipleyschool.org>
2020-04-03 16:11:01 -0400

    Finally got my simple interest table generator working!

--- amort.js
+++ amort.js
@@ -125,10 +125,10 @@
  function calculateSimpleTable()
     {
         //When the calculate button is pressed, do this:
-        var balance = parseFloat(document.getElementById("principal").value);
+        var loanAmount = parseFloat(document.getElementById("loanAmount").value);
         //divide the interest variable by 100 to get the percentage
         var interestRate = parseFloat(document.getElementById("interest").value/100.0);
-        var terms = parseInt(document.getElementById("terms").value);
+        var duration = parseInt(document.getElementById("duration").value);
 
         //set the div string 'Result'
         var div = document.getElementById("Result");
@@ -137,13 +137,13 @@
         div.innerHTML = "";
 
         //makes sure the inputs are valid and shows a message if they are not
-        var balVal = validateCompoundInputs(balance);
+        var balVal = validateCompoundInputs(loanAmount);
         var intrVal = validateCompoundInputs(interestRate);
 
         if (balVal && intrVal)
         {
             //Returns div string if inputs are valid
-            div.innerHTML += simpleAmortization(balance, interestRate, terms);
+            div.innerHTML += simpleAmortization(loanAmount, interestRate, duration);
         }
         else
         {
@@ -153,35 +153,34 @@
     }
 
 
-function simpleAmortization(balance, interestRate, terms)
+function simpleAmortization(loanAmount, interestRate, duration)
     {
         //Calculate the monthly rate by dividing by 12 (12 months)
         var monthlyRate = interestRate/12;
 
+        var interestAmount = monthlyRate * loanAmount;
+
+        var principlePmt = loanAmount / duration;
+
         //Calculate the payment
-        var payment = balance * (monthlyRate/(1-Math.pow(1+monthlyRate, -terms)));
+        var payment = interestAmount + principlePmt;
 
         //begin building the return string for the display of the amortization table
-        var result = "Loan amount: $" + balance.toFixed(2) +  "<br>" +
+        var result = "Loan amount: $" + loanAmount.toFixed(2) +  "<br>" +
             "Interest rate: " + (interestRate*100).toFixed(2) +  "%<br>" +
-            "Number of months: " + terms + "<br>" +
+            "Number of months: " + duration + "<br>" +
             "Monthly payment: $" + payment.toFixed(2) + "<br>" +
-            "Total paid: $" + (payment * terms).toFixed(2) + "<br><br>";
+            "Total paid: $" + (payment * duration).toFixed(2) + "<br><br>";
 
         //add header row for table to labels
-        result += "<table border='1'><tr><th>Month #</th><th>Balance</th>" +
-            "<th>Interest</th><th>Principal</th>";
+        result += "<table border='1'><tr><th>Month #</th><th>Beginning Balance</th>" +
+            "<th>Interest</th><th>Principal</th><th>Payment</th><th>Ending Balance</th>";
 
 
          // Loop that calculates the monthly Loan amounts
          //adds that amount to the table for displaying
-        for (var count = 0; count < terms; ++count)
+        for (var count = 0; count < duration; ++count)
         {
-            //in-loop interest amount holder
-            var interest = 0;
-
-            //in-loop monthly principal amount holder
-            var monthlyPrincipal = 0;
 
             //start a new table row on each loop iteration
             result += "<tr align=center>";
@@ -189,23 +188,27 @@ function simpleAmortization(balance, interestRate, terms)
             //display the month number in col 1 using the loop count variable
             result += "<td>" + (count + 1) + "</td>";
 
-
             //code for displaying in loop balance
-            result += "<td> $" + balance.toFixed(2) + "</td>";
+            result += "<td> $" + loanAmount.toFixed(2) + "</td>";
 
             //calc the in-loop interest amount and display
-            interest = balance * monthlyRate;
-            result += "<td> $" + interest.toFixed(2) + "</td>";
+            result += "<td> $" + interestAmount.toFixed(2) + "</td>";
 
             //calc the in-loop monthly principal and display
-            monthlyPrincipal = payment - interest;
-            result += "<td> $" + monthlyPrincipal.toFixed(2) + "</td>";
+            result += "<td> $" + principlePmt.toFixed(2) + "</td>";
+
+            result += "<td>" + payment.toFixed(2) + "</td>";
+
+            //update the balance for each loop iteration
+            loanAmount = loanAmount - principlePmt;
+
+            //ending balance
+            result += "<td>" + loanAmount.toFixed(2) + "</td>";
 
             //end the table row on each iteration of the loop
             result += "</tr>";
 
-            //update the balance for each loop iteration
-            balance = balance - monthlyPrincipal;
+
         }
 
         //closes the open table when it is the end of making the table for display
